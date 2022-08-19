44f321301e9b3c8f5cafb14a23a0843388e95ae0
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 01:32:11 +0900

    add mypage skeleton

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,8 +1,72 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import styled, { css } from "styled-components";
 import Layout from "../components/layout";
 
+enum TAB {
+  mine,
+  family,
+}
+
 const Home: NextPage = () => {
-  return <Layout>{}</Layout>;
+  const [tab, setTab] = useState(TAB.mine);
+  return (
+    <Layout>
+      <Profile>
+        <div>보호자</div>
+        <div>김보호</div>
+        <div>자식</div>
+        <div>김애깅</div>
+      </Profile>
+      <Tab>
+        <TabItem selected={tab === TAB.mine} onClick={() => setTab(TAB.mine)}>
+          내가 찜한 활동
+        </TabItem>
+        <TabItem
+          selected={tab === TAB.family}
+          onClick={() => setTab(TAB.family)}
+        >
+          가족이 찜한 활동
+        </TabItem>
+      </Tab>
+      <LikedList>
+        <LikedItem>a</LikedItem>
+        <LikedItem>a</LikedItem>
+        <LikedItem>a</LikedItem>
+        <LikedItem>a</LikedItem>
+        <LikedItem>a</LikedItem>
+      </LikedList>
+    </Layout>
+  );
 };
 
 export default Home;
+
+const Tab = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const TabItem = styled.div<{ selected: Boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${(p) =>
+    p.selected &&
+    css`
+      font-weight: bold;
+    `}
+`;
+
+const LikedList = styled.ul``;
+
+const LikedItem = styled.li``;
+
+const Profile = styled.div`
+  display: grid;
+  grid-template-columns: 5rem 1fr;
+  padding: 2rem;
+  div:nth-child(odd) {
+    font-weight: bold;
+  }
+`;
