f42851b91b1984f39664a2e804aa870d8095a692
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 01:46:02 +0900

    separate new page

--- pages/new.tsx
+++ pages/new.tsx
@@ -4,8 +4,8 @@ import Layout from "../components/layout";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div>Main Page</div>
-      <div>Issue 7</div>
+      <div>New Diary</div>
+      <div>Issue 1</div>
     </Layout>
   );
 };
