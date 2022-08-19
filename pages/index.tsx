967103a9be076269b6183027a5a30c5e79c6d954
r4bb1t <r4bb1t.github@gmail.com>
2022-08-19 22:49:01 +0900

    init layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return <Layout>ㅎㅇ</Layout>;
 };
 
 export default Home;
