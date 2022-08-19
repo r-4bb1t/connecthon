7ffd92ea7396fc8a7027814341dd67ed06ea0a1a
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-08-20 01:31:47 +0900

    Pages template based on created issues

---
+++ pages/diary.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import React from "react";
+import Layout from "../components/layout";
+
+const Diary: NextPage = () => {
+  return (
+    <Layout>
+      <div>Diaries</div>
+      <div>Issue 2</div>
+    </Layout>
+  );
+};
+
+export default Diary;
--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -2,7 +2,13 @@ import type { NextPage } from "next";
 import Layout from "../components/layout";
 
 const Home: NextPage = () => {
-  return <Layout>{}</Layout>;
+  return (
+    <Layout>
+      <div>My Page</div>
+      <div>Issue 6</div>
+      <div>Issue 4, 5</div>
+    </Layout>
+  );
 };
 
 export default Home;
