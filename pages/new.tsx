29031da5b2c743bab28058d15d67fea0963afd09
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 01:49:43 +0900

    add new diary ui

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 import Layout from "../components/layout";
 
-const Home: NextPage = () => {
+const New: NextPage = () => {
+  const router = useRouter();
+  const handleSubmit = () => {
+    router.back();
+  };
+
   return (
     <Layout>
       <div>New Diary</div>
       <div>Issue 1</div>
+      <Textarea />
+      <SubmitButton onClick={handleSubmit}>등록</SubmitButton>
     </Layout>
   );
 };
 
-export default Home;
+export default New;
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 10rem;
+`;
+
+const SubmitButton = styled.button``;
