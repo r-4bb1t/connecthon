d1ffb0c78764766a57684eb20693f5a09572eb4b
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-08-20 14:57:11 +0900

    filter page css finished temp

--- components/modal.tsx
+++ components/modal.tsx
@@ -4,6 +4,66 @@ import styled from "styled-components";
 import Datepicker from "./datepicker/datepicker";
 
 const Modal = ({ isOpen, setOpen }: any) => {
+  const city = [
+    "강남구",
+    "강동구",
+    "강북구",
+    "강서구",
+    "관악구",
+    "광진구",
+    "구로구",
+    "금천구",
+    "노원구",
+    "도봉구",
+    "동대문구",
+    "동작구",
+    "마포구",
+    "서대문구",
+    "서초구",
+    "성동구",
+    "성북구",
+    "송파구",
+    "양천구",
+    "영등포구",
+    "용산구",
+    "은평구",
+    "종로구",
+    "중구",
+    "중랑구",
+  ];
+
+  const category = [
+    "공원탐방",
+    "교육체험",
+    "문화행사",
+    "농장체험",
+    "전시관람",
+    "서울형키즈카페",
+  ];
+
+  const [selectedCity, setSelectedCity] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+
+  const handleSelectCity = (current: any) => {
+    if (!selectedCity.includes(current)) {
+      let temp: any = [...selectedCity, current];
+      setSelectedCity(temp);
+    } else {
+      let temp: any = selectedCity.filter((e) => e !== current);
+      setSelectedCity(temp);
+    }
+  };
+
+  const handleSelectCategory = (current: any) => {
+    if (!selectedCategory.includes(current)) {
+      let temp: any = [...selectedCategory, current];
+      setSelectedCategory(temp);
+    } else {
+      let temp: any = selectedCategory.filter((e) => e !== current);
+      setSelectedCategory(temp);
+    }
+  };
+
   return (
     <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
       <Sheet.Container>
@@ -11,38 +71,59 @@ const Modal = ({ isOpen, setOpen }: any) => {
         <Sheet.Content>
           <ThemesTitle>분류</ThemesTitle>
           <ThemesList>
-            <Theme>공원탐방</Theme>
-            <Theme>교육체험</Theme>
-            <Theme>문화행사</Theme>
-            <Theme>농장체험</Theme>
-            <Theme>전시관람</Theme>
-            <Theme>서울형키즈카페</Theme>
-            <Theme>공원탐방</Theme>
-            <Theme>교육체험</Theme>
-            <Theme>문화행사</Theme>
-            <Theme>농장체험</Theme>
-            <Theme>전시관람</Theme>
-            <Theme>서울형키즈카페</Theme>
+            {category.map((each: string) => {
+              if (selectedCategory.includes(each)) {
+                return (
+                  <SelectedTheme
+                    onClick={() => {
+                      handleSelectCategory(each);
+                    }}
+                  >
+                    {each}
+                  </SelectedTheme>
+                );
+              } else {
+                return (
+                  <Theme
+                    onClick={() => {
+                      handleSelectCategory(each);
+                    }}
+                    key={each}
+                  >
+                    {each}
+                  </Theme>
+                );
+              }
+            })}
           </ThemesList>
 
           <LocationsTitle>지역</LocationsTitle>
           <LocationsList>
-            <Location>강남구</Location>
-            <Location>서초구</Location>
-            <Location>마포구</Location>
-            <Location>노원구</Location>
-            <Location>관악구</Location>
-            <Location>서대문구</Location>
-            <Location>송파구</Location>
-            <Location>은평구</Location>
-            <Location>강남구</Location>
-            <Location>서초구</Location>
-            <Location>마포구</Location>
-            <Location>노원구</Location>
-            <Location>관악구</Location>
-            <Location>서대문구</Location>
-            <Location>송파구</Location>
-            <Location>은평구</Location>
+            {city.map((each: string) => {
+              if (selectedCity.includes(each)) {
+                return (
+                  <SelectedLocation
+                    onClick={() => {
+                      handleSelectCity(each);
+                    }}
+                    key={each}
+                  >
+                    {each}
+                  </SelectedLocation>
+                );
+              } else {
+                return (
+                  <Theme
+                    onClick={() => {
+                      handleSelectCity(each);
+                    }}
+                    key={each}
+                  >
+                    {each}
+                  </Theme>
+                );
+              }
+            })}
           </LocationsList>
           <DatePicker>
             <DatePickerTitle>날짜 선택</DatePickerTitle>
@@ -69,6 +150,13 @@ const ThemesList = styled.div`
   gap: 10px;
 `;
 
+const SelectedTheme = styled.div`
+  border: 1px solid black;
+  padding: 5px;
+  border-radius: 10px;
+  background-color: #fcba58;
+`;
+
 const Theme = styled.div`
   border: 1px solid black;
   padding: 5px;
@@ -87,6 +175,13 @@ const LocationsList = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   gap: 10px;
+`;
+
+const SelectedLocation = styled.div`
+  border: 1px solid black;
+  padding: 5px;
+  border-radius: 10px;
+  background-color: #fcba58;
 `;
 
 const Location = styled.div`
