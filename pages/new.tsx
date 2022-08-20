3a30e069c0720a20e2445928c066aec9cdfc469c
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 10:41:10 +0900

    add emoji button

--- pages/new.tsx
+++ pages/new.tsx
@@ -3,8 +3,11 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 import Layout from "../components/layout";
 import { useAlertContext } from "../hooks/useAlertContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTodayQuestion } from "../hooks/useTodayQuestion";
+import { AnimatePresence, motion } from "framer-motion";
+
+const emoji = ["🥰", "😭", "😛", "😋", "😎"];
 
 const New: NextPage = () => {
   const router = useRouter();
@@ -15,6 +18,9 @@ const New: NextPage = () => {
 
   const todayQuestion = useTodayQuestion();
 
+  const [emojiOpen, setEmojiOpen] = useState(false);
+  const [emojiLoading, setEmojiLoading] = useState(true);
+
   return (
     <Layout>
       <div>새 일기 쓰기</div>
@@ -24,27 +30,53 @@ const New: NextPage = () => {
         <QuestionContent>{todayQuestion.content}</QuestionContent>
       </TodayQuestion>
       <Textarea />
-      <SubmitButton onClick={handleSubmit}>등록</SubmitButton>
-      <CancleButton
-        onClick={() => {
-          push({
-            message: (
-              <>
-                정말 삭제하시겠어요?
-                <br />
-                작성한 내용이 모두 사라져요.
-              </>
-            ),
-            twoButton: true,
-            buttonText: "삭제",
-            onClose: () => {
-              router.back();
-            },
-          });
-        }}
-      >
-        취소
-      </CancleButton>
+      <ButtonContainer>
+        <SubmitButtonContainer>
+          <SubmitButton onClick={() => setEmojiOpen((s) => !s)}>
+            등록
+          </SubmitButton>
+          <AnimatePresence>
+            {emojiOpen &&
+              emoji.map((e, i) => (
+                <Emoji
+                  initial={{ x: 0, y: 0, opacity: 0 }}
+                  animate={{
+                    x: -75 * Math.cos((Math.PI / (emoji.length - 1)) * i),
+                    y: -75 * Math.sin((Math.PI / (emoji.length - 1)) * i),
+                    opacity: 1,
+                  }}
+                  exit={{ x: 0, y: 0, opacity: 0 }}
+                  onAnimationComplete={() => setEmojiLoading(false)}
+                  onAnimationStart={() => setEmojiLoading(true)}
+                  onClick={emojiLoading ? () => {} : handleSubmit}
+                  key={i}
+                >
+                  {e}
+                </Emoji>
+              ))}
+          </AnimatePresence>
+        </SubmitButtonContainer>
+        <CancleButton
+          onClick={() => {
+            push({
+              message: (
+                <>
+                  정말 삭제하시겠어요?
+                  <br />
+                  작성한 내용이 모두 사라져요.
+                </>
+              ),
+              twoButton: true,
+              buttonText: "삭제",
+              onClose: () => {
+                router.back();
+              },
+            });
+          }}
+        >
+          취소
+        </CancleButton>
+      </ButtonContainer>
     </Layout>
   );
 };
@@ -56,9 +88,38 @@ const Textarea = styled.textarea`
   height: 10rem;
 `;
 
-const SubmitButton = styled.button``;
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
 
-const CancleButton = styled(SubmitButton)``;
+const SubmitButtonContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+`;
+
+const SubmitButton = styled.button`
+  border-radius: 9999px;
+  width: 3rem;
+  height: 3rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const CancleButton = styled.button`
+  border-radius: 9999px;
+  width: 3rem;
+  height: 3rem;
+`;
 
 const TodayQuestion = styled.div`
   display: flex;
@@ -68,3 +129,15 @@ const TodayQuestion = styled.div`
 const QuestionTitle = styled.div``;
 
 const QuestionContent = styled.div``;
+
+const Emoji = styled(motion.button)`
+  position: absolute;
+  width: 3rem;
+  height: 3rem;
+  transform-origin: 1.5rem 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: lightgrey;
+  border-radius: 9999px;
+`;
