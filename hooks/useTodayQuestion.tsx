491a76f6ac04d09606c445b9c4f07ae344dccc54
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 10:00:16 +0900

    change today question logic

--- hooks/useTodayQuestion.tsx
+++ hooks/useTodayQuestion.tsx
@@ -1,4 +1,6 @@
 import question from "../constant/question.json";
 
 export const useTodayQuestion = () =>
-  question[Math.floor(new Date().getTime() / 360000) % question.length];
+  question[
+    Math.floor(new Date().getTime() / (1000 * 60 * 60 * 24)) % question.length
+  ];
