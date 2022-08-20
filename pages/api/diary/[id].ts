8d943a7b0d896ac909791468a0d94dc3aa71af7b
r4bb1t <r4bb1t.github@gmail.com>
2022-08-20 13:14:22 +0900

    add stamp

--- pages/api/diary/[id].ts
+++ pages/api/diary/[id].ts
@@ -3,10 +3,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: any, res: any) {
   res.status(200).json({
-    id: "1",
-    question: "부모님에게 서운함을 느꼈던 순간이 언제인가요?",
-    answer: " 바로 어제 서운했던 것 같기도 하네요",
-    created: "2022-07-25",
-    emotion: "https://picsum.photos/100",
+    _id: "asdf",
+    question_id: "asdf",
+    question_content: "제목",
+    is_read: false,
+    child_answer:
+      "내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용",
+    from_days: 10,
+    is_parent_answered: true,
   });
 }
--- pages/diary/[id].tsx
+++ pages/diary/[id].tsx
@@ -1,12 +1,19 @@
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useState } from "react";
+import styled, { css } from "styled-components";
 import Footer from "../../components/footer";
+import { MessageIcon } from "../../components/icons";
+import Layout from "../../components/layout";
+import { Diary } from "../../constant/types";
 
 const DiaryDetail = () => {
   const router = useRouter();
   const id = router.query.id;
 
-  const [diary, setDiary] = useState(null as any);
+  const [diary, setDiary] = useState(null as unknown as Diary);
+  const [isMessageModalOpen, setIsMessageModalOpen] = useState(false);
 
   const fetchData = useCallback(async () => {
     const result = await (
@@ -20,13 +27,91 @@ const DiaryDetail = () => {
   }, []);
 
   return (
-    <>
-      <img src={diary?.emotion} alt="emotion" />
-      <div>{diary?.question}</div>
-      <div>{diary?.answer}</div>
-      <Footer />
-    </>
+    <Layout>
+      <Main>
+        {/* <img src={diary?.emotion} alt="emotion" /> */}
+        <div>{diary?.question_content}</div>
+        <div>{diary?.child_answer}</div>
+        <StampContainer
+          isOpened={diary?.is_read}
+          onClick={() => setIsMessageModalOpen(true)}
+        >
+          <img src="/assets/stamp.png" />
+        </StampContainer>
+      </Main>
+      <AnimatePresence>
+        {isMessageModalOpen && (
+          <ModalBackground onClick={() => setIsMessageModalOpen(false)}>
+            <Modal
+              initial={{ opacity: 0, y: 100, scale: 0.9 }}
+              animate={{ opacity: 1, y: 0, scale: 1 }}
+              exit={{ opacity: 0, y: 100, scale: 0.9 }}
+            ></Modal>
+          </ModalBackground>
+        )}
+      </AnimatePresence>
+    </Layout>
   );
 };
 
 export default DiaryDetail;
+
+const Main = styled.main`
+  position: relative;
+  padding-bottom: 12rem;
+`;
+
+const StampContainer = styled.div<{ isOpened: boolean }>`
+  position: fixed;
+  right: 0;
+  bottom: 4rem;
+  width: 10rem;
+  height: 10rem;
+  padding: 2rem;
+  @keyframes bounce2 {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-15px);
+    }
+    60% {
+      transform: translateY(-10px);
+    }
+  }
+  ${(p) =>
+    !p.isOpened &&
+    css`
+      animation: bounce2 2s ease infinite;
+    `}
+  img {
+    width: 100%;
+  }
+`;
+
+const ModalBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(10px);
+`;
+
+const Modal = styled(motion.div)`
+  min-height: 100vh;
+  background-color: white;
+  position: fixed;
+  top: 5rem;
+  bottom: 0;
+  left: 1rem;
+  right: 1rem;
+`;
